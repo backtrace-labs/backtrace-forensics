c8edcc9d07d5a25d53873008dc2a699b033aa9df
Sebastian Alex <sebastian.alex@saucelabs.com>
2023-02-13 16:27:08 +0100

    Adds query printing examples.

---
+++ backtrace-forensics-sample/src/examples/select-print-query.ts
@@ -0,0 +1,10 @@
+import { Filters } from 'backtrace-forensics';
+import { instance } from './instance';
+
+const query = instance.filter('timestamp', Filters.time.from.last.days(7).to.now()).select('callstack', 'version');
+
+const queryObj = query.json();
+
+// You can post this exact request to Coroner and it will return the same values as Forensics
+const json = JSON.stringify(queryObj, null, '\t');
+console.log(json);
