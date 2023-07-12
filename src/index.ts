1fb1d32a810417e3ba476e8a91fa495449c404d4
Sebastian Alex <sebastian.alex@saucelabs.com>
2023-07-12 14:44:50 +0000

    base: export additional stuff from backtrace-forensics

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export * from './BacktraceForensics';
 export * from './common';
 export * from './coroner';
 export * from './filters';
+export * from './implementation/helpers';
 export * from './interfaces';
 export * from './models';
 export default BacktraceForensics;
