a561b08784a15443b0ce2831ea8d9cf1fec26d82
Sebastian Alex <sebastian.alex@saucelabs.com>
2023-07-12 14:34:34 +0000

    ticket models: change assignee model

--- src/coroner/models/Ticket.ts
+++ src/coroner/models/Ticket.ts
@@ -1,11 +1,15 @@
-import { User } from './User';
-
 export type TicketState = 'open' | 'muted' | 'resolved' | 'in-progress';
+
+export interface TicketAssignee {
+    id: number;
+    username: string;
+    email: string;
+}
 
 export interface Ticket {
     watcher: string;
-    assignees?: User[];
-    state: string;
+    assignees?: TicketAssignee[];
+    state: TicketState;
     short: string;
     url: string;
 }
